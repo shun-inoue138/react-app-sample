ca2c4b72ab6c038265413187cdb230a29dddf8c0
shun-inoue138 <ht8sk13@icloud.com>
2021-01-25 18:00:49 +0900

    複数stateのおためし

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React, {useState} from 'react';
 
 
 const App = () =>{
-  const[count,setCount] =useState(0);
-  const upCount = () => setCount(count+1);
-  const divisionThree = () => setCount(previousCount => {
-    if(previousCount % 3 === 0){return previousCount / 3;}else{return previousCount}
-  })
-  return(
+  const initialState ={
+    name :'サンプル',
+    price:1000
+  }
+  const [name,setName] = useState(initialState.name);
+  const [price,setPrice] = useState(initialState.price);
+  return (
   <>
-    <div>
-      count:{count}
-    </div>
-    <button onClick={upCount}>数字が増えます</button>
-    <button onClick={divisionThree}>3の倍数のとき商を出します</button>
-
+    <p >現在の{name}は{price}円です</p>
+    <button onClick={() => setPrice(price +1)}>1円値上げ</button>
+    <button onClick={() => setPrice(price -1)}>1円値下げ</button>
+    <input value={name}　onChange={e => {console.log(e);setName(e.target.value);}}></input>
   </>
   )
 }
