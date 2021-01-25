7dc0b448aaedd872004aa2d87af3647791ad2ef6
shun-inoue138 <ht8sk13@icloud.com>
2021-01-25 17:29:30 +0900

    useStateのチュートリアル

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const App = () =>{
+  const[count,setCount] =useState(0);
+  const upCount = () => setCount(count+1);
+  const divisionThree = () => setCount(previousCount => {
+    if(previousCount % 3 === 0){return previousCount / 3;}else{return previousCount}
+  })
   return(
+  <>
     <div>
-      This is a template for React App！
+      count:{count}
     </div>
+    <button onClick={upCount}>数字が増えます</button>
+    <button onClick={divisionThree}>3の倍数のとき商を出します</button>
+
+  </>
   )
 }
 
